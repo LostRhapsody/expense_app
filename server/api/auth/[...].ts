db6f38840e53f48f44e95013967ee44fd71f64c6
Evan Robertson <evan.robertson77@gmail.com>
2024-02-04 14:17:00 -0500

    Removed github as sign in option.

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -1,5 +1,4 @@
 // file: ~/server/api/auth/[...].ts
-import GithubProvider from 'next-auth/providers/github'
 import GoogleProvider from 'next-auth/providers/google'
 import { NuxtAuthHandler } from '#auth'
 
@@ -7,11 +6,6 @@ export default NuxtAuthHandler({
   // A secret string you define, to ensure correct encryption
   secret: process.env.AUTH_SECRET,
   providers: [
-    // @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point
-    GithubProvider.default({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET
-    }),
     GoogleProvider.default({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET
